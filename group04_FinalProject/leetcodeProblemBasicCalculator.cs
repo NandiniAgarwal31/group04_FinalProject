b9c06d9efad67c50a3a1ccf09c856faa6fdb2c09
Mahika Gunjkar <gunjkamg@mail.uc.edu>
2024-04-19 19:49:08 -0400

    made changes Basic calculator

--- group04_FinalProject/leetcodeProblemBasicCalculator.cs
+++ group04_FinalProject/leetcodeProblemBasicCalculator.cs
@@ -1,67 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Text;
 
 namespace group04_FinalProject
 {
-    public class leetcodeProblemBasicCalculator
+    public class LeetcodeProblemBasicCalculator
     {
-            public int Calculate(string s)
+        public int Calculate(string s)
+        {
+            int result = 0;
+            int i = 0;
+            Stack<int> stack = new Stack<int>();
+            int sign = 1;
+
+            while (i < s.Length)
             {
-                int result = 0;
-                int i = 0;
-                Stack<int> stack = new Stack<int>();
-                int sign = 1;
-                int num = 0;
-                while (i < s.Length)
+                char c = s[i];
+                if (c == '+')
                 {
-                    switch (s[i])
-                    {
-                        case '+':
-                            sign = 1;
-                            break;
-                        case '-':
-                            sign = -1;
-                            break;
-                        case '(':
-                            stack.Push(result);
-                            stack.Push(sign);
-                            result = 0;
-                            sign = 1;
-                            break;
-                        case ')':
-                            int lastSign = stack.Pop();
-                            result *= lastSign;
-                            int lastNum = stack.Pop();
-                            result += lastNum;
-
-                            break;
-                        case ' ':
-                            break;
-                        default:
-                            if (char.IsDigit(s[i]))
-                            {
-                                StringBuilder stringBuilder = new StringBuilder();
-                                stringBuilder.Append(s[i]);
-                                while (i + 1 < s.Length && char.IsDigit(s[i + 1]))
-                                {
-                                    stringBuilder.Append(s[++i]);
-                                }
-
-                                num = int.Parse(stringBuilder.ToString());
-                                num *= sign;
-                                result += num;
-                                sign = 1;
-                            }
-                            break;
-                    }
+                    sign = 1;
                     i++;
                 }
-
-                return result;
+                else if (c == '-')
+                {
+                    sign = -1;
+                    i++;
+                }
+                else if (c == '(')
+                {
+                    // Push the result and the sign onto the stack
+                    stack.Push(result);
+                    stack.Push(sign);
+                    // Reset the result and sign for the new sub-expression
+                    result = 0;
+                    sign = 1;
+                    i++;
+                }
+                else if (c == ')')
+                {
+                    // Pop the sign and apply it to the result
+                    result *= stack.Pop();
+                    // Add the result to the next operand on the top of the stack
+                    result += stack.Pop();
+                    i++;
+                }
+                else if (c == ' ')
+                {
+                    // Skip spaces
+                    i++;
+                }
+                else
+                {
+                    // If it's a digit, build the number and then add it to the result
+                    int num = 0;
+                    while (i < s.Length && char.IsDigit(s[i]))
+                    {
+                        num = num * 10 + (s[i] - '0');
+                        i++;
+                    }
+                    result += sign * num;
+                }
             }
 
-        
+            return result;
+        }
     }
 }
