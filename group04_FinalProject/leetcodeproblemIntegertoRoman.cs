fd2c371d586efe192ced18517bbdc1c4e0a621c8
Mahika Gunjkar <gunjkamg@mail.uc.edu>
2024-04-20 18:55:10 -0400

    Changes in default.apsx

--- group04_FinalProject/leetcodeproblemIntegertoRoman.cs
+++ group04_FinalProject/leetcodeproblemIntegertoRoman.cs
@@ -8,45 +8,28 @@ namespace group04_FinalProject
     public class leetcodeproblemIntegertoRoman
     {
         public string IntToRoman(int num)
-
         {
             // Arrays for Roman numeral symbols and their corresponding values
-
             string[] symbols = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
-
             int[] values = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
 
-
-
-            StringBuilder romanNumeral = new StringBuilder();
+            string romanNumeral = ""; // Initialize an empty string for the result
 
             // Iterate through the values array
-
             for (int i = 0; i < values.Length; i++)
-
             {
-
                 // While the given number is greater than or equal to the current value
-
                 while (num >= values[i])
-
                 {
-
                     // Append the corresponding symbol to the result
-
-                    romanNumeral.Append(symbols[i]);
+                    romanNumeral += symbols[i];
 
                     // Subtract the value from the given number
-
                     num -= values[i];
-
                 }
-
             }
 
-
-
-            return romanNumeral.ToString();
+            return romanNumeral;
         }
     }
 }
