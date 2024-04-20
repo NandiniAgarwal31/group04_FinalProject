5445b2d62d0014db8edad3d6e087436eb0692c21
Mahika Gunjkar <gunjkamg@mail.uc.edu>
2024-04-19 20:22:39 -0400

    git changestrying to figure out the git hub pull and push

--- group04_FinalProject/default.aspx.designer.cs
+++ group04_FinalProject/default.aspx.designer.cs
@@ -22,5 +22,14 @@ namespace group04_FinalProject
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// ListBox1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListBox ListBox1;
     }
 }
