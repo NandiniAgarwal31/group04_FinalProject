a77ab9dfc922ae41c2f2fdd95d28131a02b9223a
Nandini Agarwal <nandiniagarwal1710@gmail.com>
2024-04-22 13:46:39 -0400

    Made changes to interger to roman.

--- group04_FinalProject/leetcodeproblemIntegertoRoman.cs
+++ group04_FinalProject/leetcodeproblemIntegertoRoman.cs
@@ -9,22 +9,22 @@ namespace group04_FinalProject
     {
         public string IntToRoman(int num)
         {
-            // Arrays for Roman numeral symbols and their corresponding values
+            
             string[] symbols = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
             int[] values = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
 
-            string romanNumeral = ""; // Initialize an empty string for the result
+            string romanNumeral = ""; 
 
-            // Iterate through the values array
+            
             for (int i = 0; i < values.Length; i++)
             {
-                // While the given number is greater than or equal to the current value
+                
                 while (num >= values[i])
                 {
-                    // Append the corresponding symbol to the result
+                    
                     romanNumeral += symbols[i];
 
-                    // Subtract the value from the given number
+                    
                     num -= values[i];
                 }
             }
